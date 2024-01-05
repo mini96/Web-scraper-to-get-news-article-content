fc3b0bcc0ac18308d3244d84bfaafddff42bea33
mini96 <49809307+mini96@users.noreply.github.com>
2024-01-05 23:31:26 +0100

    Commented and added main body content on display

--- scrape_webarticle.py
+++ scrape_webarticle.py
@@ -2,21 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
+# The below variable collects the URL Argument the users enter when running this script
 URL = sys.argv[1]
 page = requests.get(URL)
 
 
-
+# Soup is the variable used to store all the html content
 soup = BeautifulSoup(page.content, "html.parser")
 
+# Below are the variables for the respective items requested to display from the website
 headline = soup.find("title")
 timestamp = soup.find(attrs={'class':'timestamp'})
+content = soup.find(attrs={'class':'article__content-container'})
 
-content_div = soup.find(id="ResultsContainer")
+# Printing the items with formatting
+## content_div = soup.find(id="ResultsContainer")
 print("**** Title of the web article is ****")
 print()
 print(headline.text)
 print()
 print("**** Timestamp ****")
 print(timestamp.text)
+print()
+print("**** Content ****")
+print(content.text)
 
